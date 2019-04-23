3006319c233514c76cc8b780c3ded2082e427b4a
Anders Kensby <anders.kensby@gmail.com>
2019-04-23 15:23:27 +0200

    initial commit

---
+++ tooltip.js
@@ -0,0 +1,47 @@
+class Tooltip extends HTMLElement {
+    constructor() {
+        super();
+        this._tooltipContainer;
+        this._tooltipText = "default text";
+        this.attachShadow({ mode: 'open' });
+        this.shadowRoot.innerHTML = `
+            <style>
+                div {
+                    background-color: black;
+                    color: white;
+                    position: absolute;
+                }
+
+                ::slotted(.highlight) {
+                    background-color: orange;
+                }
+            </style>
+            <slot>
+                Slot default
+            </slot>
+            <span> (?)</span>
+        `;
+    }
+
+    connectedCallback() {
+        if(this.hasAttribute('text')) {
+            this._tooltipText = this.getAttribute('text');
+        }
+        const tooltipIcon = this.shadowRoot.querySelector('span');
+        tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this) );
+        tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this) );
+        this.shadowRoot.appendChild(tooltipIcon);
+    }
+
+    _showTooltip = () => {
+        this._tooltipContainer = document.createElement('div');
+        this._tooltipContainer.textContent = this._tooltipText;
+        this.shadowRoot.appendChild(this._tooltipContainer);
+    }
+
+    _hideTooltip = () => {
+        this.shadowRoot.removeChild(this._tooltipContainer);
+    }
+}
+
+customElements.define('my-tooltip', Tooltip);
